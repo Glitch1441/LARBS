f262db7d2b56b85b6dc3d831ae6a78933c35da13
Glitch1441 <110949634+Glitch1441@users.noreply.github.com>
2023-01-26 09:21:24 +0000

    Update larbs.sh

--- static/larbs.sh
+++ static/larbs.sh
@@ -6,8 +6,8 @@
 
 ### OPTIONS AND VARIABLES ###
 
-dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/static/progs.csv"
+dotfilesrepo="https://github.com/Glitch1441/voidrice.git"
+progsfile="https://raw.githubusercontent.com/Glitch1441/LARBS/master/static/progs.csv"
 aurhelper="yay"
 repobranch="master"
 
